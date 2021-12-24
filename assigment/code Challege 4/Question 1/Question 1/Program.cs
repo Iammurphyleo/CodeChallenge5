910ff33eede4d6bf6ce7a6e76e8c964197ec3d61
Iammurphyleo <dadamuftau@gmail.com>
2021-12-24 13:23:21 +0100

    code challenge

---
+++ assigment/code Challege 4/Question 1/Question 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+            int[] number = {2, 5, 6, 7, 8, 9, 12 };
+
+            int T = 8;
+
+            ReturnTarget(number, T);
+        
+        
+        
+        }
+        public static void ReturnTarget(int[] number,int T ) 
+        {
+            int check = 0;
+            for (int i = 0; i < number.Length; i++) 
+            {
+                if (T == number[i])
+                {
+                    Console.Write($"{i},");
+                }
+                else 
+                {
+                    check++;
+                }
+            }
+            if ( check == number.Length) 
+            {
+                Console.WriteLine("-1, -1");
+            }
+        }
+
+   
+    
+    
+    }
+}
